e84cb0fc89d62536bab6576d39efab8c1787bbca
David Ziegler <DavidZieglerMail@gmail.com>
2019-05-29 23:06:49 +0200

    Add toolings
    Image resize

---
+++ first_init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#[ "$UID" -eq 0 ] || exec kdesu bash "$0" "$@"
+PS4='$LINENO: '
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+echo "Pull submodules"
+git submodule update --init --recursive
+
+echo "Extract image"
+cat $(find "$DIR/images/" -name "django_2.2.1.tar.gz*") > "$DIR/images/django_2.2.1.tar.gz" 
+gzip -d "$DIR/images/django_2.2.1.tar.gz"
+rm "$DIR/images/django_2.2.1.tar.gz" 
+
+echo "Set links"
+rm -rf "$DIR/.docker_toolbox/mounts"
+rm -rf "$DIR/.docker_toolbox/images"
+ln -s  "$DIR/.docker_toolbox/" "$DIR"
+ln -s  "$DIR/mounts" "$DIR/.docker_toolbox/mounts"
+ln -s  "$DIR/images" "$DIR/.docker_toolbox/images"
+
+echo "Load image"
+"$DIR/scripts/docker_load_image.sh" "$DIR/images/django_2.2.1.tar"
---
+++ run_django.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+bash $DIR/scripts/docker_exec.sh "/usr/bin/python3 /home/docker/code/app/manage.py runserver 0.0.0.0:8000"
